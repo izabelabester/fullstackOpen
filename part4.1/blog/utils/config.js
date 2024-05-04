bc9b6f2432829d0a9c11d699554389688032e15f
Izabela B <izabester@gmail.com>
2024-05-04 16:18:07 +0100

    config update

--- part4.1/blog/utils/config.js
+++ part4.1/blog/utils/config.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 let PORT = 3003;
 // process.env.PORT
-let MONGODB_URI = 'mongodb+srv://izabester1:OLCFr0zRzvMPMhh4@cluster0.o4bh3qk.mongodb.net/?retryWrites=true&w=majority'
+let MONGODB_URI = 'mongodb+srv://db-user:{password}@cluster0.o4bh3qk.mongodb.net/?retryWrites=true&w=majority'
 // `mongodb+srv://izabester1:${password}@cluster0.o4bh3qk.mongodb.net/noteApp?retryWrites=true&w=majority`
 // process.env.MONGODB_URI
 
